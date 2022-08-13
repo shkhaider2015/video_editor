76858d45f13c6bbc5dce96ab2363b9a1d16e3940
Shakeel Haider <shkhaider2015@gmail.com>
2022-08-14 00:05:54 +0500

    add video validations

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import re
-import os
+import utils
 
 
 
-def fun(start_time, end_time, fileName):
-    clip2 = VideoFileClip(fileName).subclip(start_time,end_time)
-    clip2.write_videofile("new.mp4")
+def fun(inputs, fileName):
+    clips = []
+    for item in inputs:
+      clip = VideoFileClip(fileName).subclip(item['start_time'], item['end_time'])
+      clips.append(clip)
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile("new.mp4")
 
 
-
-start_time = input("Enter start time (e.g: hh:mm:ss) : ") or '00:00:05'
-end_time = input("Enter end time (e.g: hh:mm:ss) : ") or '00:00:10'
 pattern = re.compile(r'([0-1]?\d|2[0-3])(?::([0-5]?\d))(?::([0-5]?\d))')
-start_result = pattern.search(start_time)
-end_result = pattern.search(start_time)
+filename = input("Drag and drop video file here : ")
 
-if start_result == None:
-    raise Exception("Plase enter valid Start Time e.g 01:12:33 ")
-if end_result == None:
-    raise Exception("Plase enter valid End Time e.g 01:12:33 ")
-path = str(os.getcwdb())
+if not (utils.is_video_file(filename)):
+  raise Exception("Sorry This is not video file")
+
+break_input = False
+inputs = []
+while not break_input:
+    si = input("Enter start time (e.g: hh:mm:ss) : ")
+    ei = input("Enter end time (e.g: hh:mm:ss) : ")
+    start_result = pattern.search(si)
+    end_result = pattern.search(ei)
+
+    if start_result == None:
+      raise Exception("Plase enter valid Start Time e.g 01:12:33 ")
+    if end_result == None:
+      raise Exception("Plase enter valid End Time e.g 01:12:33 ")
+
+    inputs.append({ 'start_time': si, 'end_time': ei })
+    q = input("Want to added more points ? y: Yes, n: No ")
+    if q == 'n' or q == 'no':
+        break_input = True
+
+
+
 try:
-  fun(start_time, end_time, '100seconds.mp4')
+  fun(inputs, filename)
 except Exception as e:
   print(f'An exception occurred : {e}')
 
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-from os import getcwdb
-path = str(getcwdb())
-print(path+'\\files')
+
+import os
+
+br = False
+while not br:
+    print("pp")
+    x = input("OO : ")
+    if x == 'y':
+        br = True
